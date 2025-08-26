6f8e42b22dba62851c58d324ac41a9a7ff2de3a6
ez3903 <ez3903@gmail.com>
2025-08-26 23:06:05 +0800

    feat: update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,5 @@
-import { Stack, Screen } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Stack } from "expo-router";
+import { StyleSheet } from "react-native";
 
 const RootLayout = () => {
   return (
